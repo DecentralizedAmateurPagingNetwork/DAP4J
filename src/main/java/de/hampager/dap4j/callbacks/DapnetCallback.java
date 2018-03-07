205b193eb072fb1f8c0cc33780587f621a226eec
Schwarz <schwarz@ihf.rwth-aachen.de>
2018-03-07 10:41:33 +0100

    Callback to be implemented by client

--- src/main/java/de/hampager/dap4j/callbacks/DapnetCallback.java
+++ src/main/java/de/hampager/dap4j/callbacks/DapnetCallback.java
@@ -2,17 +2,11 @@ package de.hampager.dap4j.callbacks;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
-public class DapnetCallback implements Callback<ResponseBody> {
-    @Override
-    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+public interface DapnetCallback {
 
-    }
+    void onResponse(Call<ResponseBody> call, Response<ResponseBody> response);
 
-    @Override
-    public void onFailure(Call<ResponseBody> call, Throwable t) {
-
-    }
+    void onFailure(Call<ResponseBody> call, Throwable t);
 }
